824c1659e78eced009207fb0e2133be16fa1ef6c
Isfar Baset <baseti@mail.gvsu.edu>
2020-02-20 15:19:47 -0500

    Started Part A

---
+++ src/main/java/com/example/calculatorapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.calculatorapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.support.design.widgets.Snackbar;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
